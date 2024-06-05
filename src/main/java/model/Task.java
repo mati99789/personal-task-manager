c0606c3e2c52881f2344eab8238df2ad1d7e1961
Mateusz Urbaniak <matteus.urbaniak@hotmail.com>
2024-06-05 18:15:29 +0200

    Add basic validation, provide information for Task class, add UUID for id.

--- src/main/java/model/Task.java
+++ src/main/java/model/Task.java
@@ -1,9 +1,15 @@
 package model;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
+/**
+ * Represent a task in the Personal Task Manager
+ * Each task has a unique identifier, title, description,
+ * due date, priority level, and current status.
+ */
 public class Task {
-	 private long id;
+	private UUID id;
 	private String title;
 	private String description;
 	private LocalDate dueDate;
@@ -13,7 +19,15 @@ public class Task {
 	public Task() {
 	}
 
+	public Task(String title, String description, LocalDate dueDate) {
+		this(title, description, dueDate, Priority.MEDIUM, model.Status.TODO);
+		this.title = title;
+		this.description = description;
+		this.dueDate = dueDate;
+	}
+
 	public Task(String title, String description, LocalDate dueDate, Priority priority, model.Status status) {
+		this.id = UUID.randomUUID();
 		this.title = title;
 		this.description = description;
 		this.dueDate = dueDate;
@@ -21,27 +35,23 @@ public class Task {
 		Status = status;
 	}
 
-	public long getId() {
+	public UUID getId() {
 		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
 	}
 
 	public String getTitle() {
 		return title;
 	}
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
 
 	public String getDescription() {
 		return description;
 	}
 
 	public void setDescription(String description) {
+		if(description == null || description.trim().isEmpty()) {
+			throw new IllegalArgumentException("Description cannot be null or empty");
+		}
 		this.description = description;
 	}
 
@@ -50,6 +60,9 @@ public class Task {
 	}
 
 	public void setDueDate(LocalDate dueDate) {
+		if(dueDate == null || dueDate.isBefore(LocalDate.now())) {
+			throw new IllegalArgumentException("Due date cannot be null or in the past");
+		}
 		this.dueDate = dueDate;
 	}
 
@@ -68,6 +81,7 @@ public class Task {
 	public void setStatus(model.Status status) {
 		Status = status;
 	}
+
 
 	@Override
 	public String toString() {
